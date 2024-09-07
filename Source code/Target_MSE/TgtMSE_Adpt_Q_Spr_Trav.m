4253de59163709503559727a86665e419a7fddcb
Thisura24 <tisura.e@gmail.com>
2024-09-08 02:54:57 +0530

    Added Files

---
+++ Source code/Target_MSE/TgtMSE_Adpt_Q_Spr_Trav.m
@@ -0,0 +1,595 @@
+clc
+clear all
+close all
+
+% Matlab script for outputing compression metrics for a given 16bit DICOM
+% image and a target MSE value. Here Adaptive quantization with Spiral
+% Traversal is used. File path and target MSE value should be given as
+% input parameters.
+
+
+% Start the timer
+tic
+
+%input Parameters
+fileName = ' ';
+targetMSE = 225;
+
+s = dir(fileName);
+orgSize = s.bytes;
+
+img = dicomread(fileName);
+class = class(img);
+
+if strcmp(class, 'uint16')
+
+    dicomClass = @uint16;
+
+elseif strcmp(class, 'int16')
+
+    dicomClass = @int16;
+
+else
+    error('Error: Invalid DICOM class. Only supports uint16 and int16');
+end
+
+
+metadata = dicominfo(fileName);
+
+w = 'db1';
+n = 1;
+[C,S] = wavedec2(img,n,w);
+
+Csort = sort(abs(C(:)));
+
+A1 = appcoef2(C,S,w,1);
+[H1 V1 D1] = detcoef2('all',C,S,1);
+
+
+iterationMat = [];
+MSEMat = [];
+peaksnrMat = [];
+absDiffMat = [];
+compressionRatioMat = [];
+encMatSizeMat = [];
+
+
+A1filt = [];
+H1filt = [];
+V1filt = [];
+D1filt = [];
+MSE = 0;
+iteration = 0;
+q = 0;
+
+
+exitOuterLoop = false;
+reEval = true;
+startThresh = 0.20;
+startQunt = 0;
+EndQunt = 0;
+
+
+if targetMSE<=20
+
+    startQunt = 20;
+    EndQunt = 1;
+
+elseif targetMSE<=50
+
+    startQunt = 30;
+    EndQunt = 10;
+
+elseif targetMSE<=100
+
+    startQunt = 40;
+    EndQunt = 20;
+
+elseif targetMSE<=200
+
+    startQunt = 50;
+    EndQunt = 25;
+
+else
+    startThresh = 0.10;
+    startQunt = 60;
+    EndQunt = 20;
+
+end
+
+endThresh = 0.99;
+
+for i = 1:4
+
+    MSEMat = [];
+    middleThresh = (startThresh + endThresh)/2;
+
+    keep = [startThresh , middleThresh ];
+
+    for i = 1:length(keep)
+        keepValue = keep(i);
+
+        thresh = Csort(round((1-keepValue)*length(Csort)));
+
+        q = EndQunt;
+
+        indA1 = abs(A1)>thresh;
+        indH1 = abs(H1)>thresh;
+        indV1 = abs(V1)>thresh;
+        indD1 = abs(D1)>thresh;
+
+        A1filt = A1.*indA1;
+        H1filt = H1.*indH1;
+        V1filt = V1.*indV1;
+        D1filt = D1.*indD1;
+
+
+        A1filt1 = round(A1filt/EndQunt);
+        H1filt1 = round(H1filt/EndQunt);
+        V1filt1 = round(V1filt/EndQunt);
+        D1filt1 = round(D1filt/EndQunt);
+
+        A1filtNew = A1filt1 * EndQunt;
+        V1filtNew = V1filt1 * EndQunt;
+        H1filtNew = H1filt1 * EndQunt;
+        D1FiltNew = D1filt1 * EndQunt;
+
+
+        reconstructedImage = dicomClass(idwt2(A1filtNew,H1filtNew,V1filtNew,D1FiltNew,w));
+
+        MSEMat = [MSEMat immse(img,reconstructedImage)];
+
+    end
+
+    if MSEMat(1) >= targetMSE && MSEMat(2) <= targetMSE
+
+        endThresh = middleThresh;
+
+    else
+
+        startThresh = middleThresh;
+
+    end
+
+end
+
+
+startThresh = floor(startThresh*100) / 100;
+endThresh = ceil(endThresh*100) / 100;
+
+for n= startThresh: 0.001 : endThresh
+
+    keep = [n];
+    iteration = n;
+
+    thresh = Csort(round((1-keep)*length(Csort)));
+    q = startQunt;
+
+    indA1 = abs(A1)>thresh;
+    indH1 = abs(H1)>thresh;
+    indV1 = abs(V1)>thresh;
+    indD1 = abs(D1)>thresh;
+
+    A1filt = A1.*indA1;
+    H1filt = H1.*indH1;
+    V1filt = V1.*indV1;
+    D1filt = D1.*indD1;
+
+    A1filt1 = round(A1filt/startQunt);
+    H1filt1 = round(H1filt/startQunt);
+    V1filt1 = round(V1filt/startQunt);
+    D1filt1 = round(D1filt/startQunt);
+
+    A1filtNew = A1filt1 * startQunt;
+    V1filtNew = V1filt1 * startQunt;
+    H1filtNew = H1filt1 * startQunt;
+    D1FiltNew = D1filt1 * startQunt;
+
+    reconstructedImage = dicomClass(idwt2(A1filtNew,H1filtNew,V1filtNew,D1FiltNew,w));
+    MSE_atStartQ = immse(img,reconstructedImage);
+
+    A1filt2 = round(A1filt/EndQunt);
+    H1filt2 = round(H1filt/EndQunt);
+    V1filt2 = round(V1filt/EndQunt);
+    D1filt2 = round(D1filt/EndQunt);
+
+    A1filtNew = A1filt2 * EndQunt;
+    V1filtNew = V1filt2 * EndQunt;
+    H1filtNew = H1filt2 * EndQunt;
+    D1FiltNew = D1filt2 * EndQunt;
+
+    reconstructedImage = dicomClass(idwt2(A1filtNew,H1filtNew,V1filtNew,D1FiltNew,w));
+    MSE_atEndQ = immse(img,reconstructedImage);
+
+    if MSE_atStartQ>=targetMSE && targetMSE>=MSE_atEndQ
+
+        for p= startQunt: -1 : EndQunt
+
+            A1filtQ = round(A1filt/q);
+            H1filtQ = round(H1filt/q);
+            V1filtQ = round(V1filt/q);
+            D1filtQ = round(D1filt/q);
+
+            A1filtNew = A1filtQ * q;
+            V1filtNew = V1filtQ * q;
+            H1filtNew = H1filtQ * q;
+            D1FiltNew = D1filtQ * q;
+
+            reconstructedImage = dicomClass(idwt2(A1filtNew,H1filtNew,V1filtNew,D1FiltNew,w));
+            MSE = immse(img,reconstructedImage);
+
+            if MSE>=targetMSE-0.1 && MSE<=targetMSE+0.1
+                exitOuterLoop = true;
+                reEval = false;
+                break;
+
+            end
+
+            q = q - 1;
+
+        end
+
+    end
+
+    if exitOuterLoop==true
+        break;
+    end
+
+end
+
+
+if reEval == true && EndQunt > 10
+
+    startQunt = EndQunt;
+    EndQunt = EndQunt - 10;
+
+    for n= startThresh: 0.001 : endThresh
+
+        keep = [n];
+        iteration = n;
+
+        thresh = Csort(round((1-keep)*length(Csort)));
+
+        q = startQunt;
+
+        indA1 = abs(A1)>thresh;
+        indH1 = abs(H1)>thresh;
+        indV1 = abs(V1)>thresh;
+        indD1 = abs(D1)>thresh;
+
+        A1filt = A1.*indA1;
+        H1filt = H1.*indH1;
+        V1filt = V1.*indV1;
+        D1filt = D1.*indD1;
+
+        A1filt1 = round(A1filt/startQunt);
+        H1filt1 = round(H1filt/startQunt);
+        V1filt1 = round(V1filt/startQunt);
+        D1filt1 = round(D1filt/startQunt);
+
+        A1filtNew = A1filt1 * startQunt;
+        V1filtNew = V1filt1 * startQunt;
+        H1filtNew = H1filt1 * startQunt;
+        D1FiltNew = D1filt1 * startQunt;
+
+        reconstructedImage = dicomClass(idwt2(A1filtNew,H1filtNew,V1filtNew,D1FiltNew,w));
+        MSE_atStartQ = immse(img,reconstructedImage);
+
+        A1filt2 = round(A1filt/EndQunt);
+        H1filt2 = round(H1filt/EndQunt);
+        V1filt2 = round(V1filt/EndQunt);
+        D1filt2 = round(D1filt/EndQunt);
+
+        A1filtNew = A1filt2 * EndQunt;
+        V1filtNew = V1filt2 * EndQunt;
+        H1filtNew = H1filt2 * EndQunt;
+        D1FiltNew = D1filt2 * EndQunt;
+
+        reconstructedImage = dicomClass(idwt2(A1filtNew,H1filtNew,V1filtNew,D1FiltNew,w));
+        MSE_atEndQ = immse(img,reconstructedImage);
+
+        if MSE_atStartQ>=targetMSE && targetMSE>=MSE_atEndQ
+
+            for p= startQunt: -1 : EndQunt
+
+                A1filtQ = round(A1filt/q);
+                H1filtQ = round(H1filt/q);
+                V1filtQ = round(V1filt/q);
+                D1filtQ = round(D1filt/q);
+
+                A1filtNew = A1filtQ * q;
+                V1filtNew = V1filtQ * q;
+                H1filtNew = H1filtQ * q;
+                D1FiltNew = D1filtQ * q;
+
+                reconstructedImage = dicomClass(idwt2(A1filtNew,H1filtNew,V1filtNew,D1FiltNew,w));
+
+                MSE = immse(img,reconstructedImage);
+
+                if MSE>=targetMSE-0.1 && MSE<=targetMSE+0.1
+                    exitOuterLoop = true;
+                    reEval = false;
+                    break;
+
+                end
+                q = q - 1;
+
+            end
+
+        end
+
+        if exitOuterLoop==true
+            break;
+        end
+
+    end
+
+end
+
+A1filt = round(A1filt/q);
+H1filt = round(H1filt/q);
+V1filt = round(V1filt/q);
+D1filt = round(D1filt/q);
+
+reshapedA1 = reshape(A1filt, 1 , []);
+
+VHD = [V1filt H1filt D1filt];
+
+reshapedVHD = spiralTraversal(VHD);
+
+rleEncodedVHD = run_length_encode(reshapedVHD,'encode');
+
+CombinedMatrix = [reshapedA1 rleEncodedVHD];
+
+rowsA1 = size(A1filt,1);
+
+coloumnsA1  = size(A1filt,2);
+
+currentDir = pwd;
+
+[~, fileName, ~] = fileparts(fileName);
+
+CompFolder = strcat(fileName , '_Compressed');
+
+if exist(CompFolder, 'dir')
+    rmdir(CompFolder, 's');  % Remove existing folder and its contents
+end
+
+
+mkdir(CompFolder);
+
+cd(CompFolder);
+
+save("metadata.mat","q","metadata","rowsA1","coloumnsA1");
+
+writematrix(CombinedMatrix);
+
+gzip('CombinedMatrix.txt')
+
+openfile = fopen("CombinedMatrix.txt.gz","r");
+
+data = fread(openfile);
+
+OpenDatafileID = fopen('ImgData1.bin', 'w');
+
+fwrite(OpenDatafileID, data);
+
+% Close the file
+fclose(OpenDatafileID);
+fclose(openfile);
+
+delete('CombinedMatrix.txt',"CombinedMatrix.txt.gz");
+
+
+%---------------Decompression---------------------------
+
+gunzip('ImgData1.bin')
+
+CombinedMatrixRead = readmatrix('ImgData1');
+
+load('metadata.mat');
+
+colsComMat = size(CombinedMatrix,2);
+
+A1ext = CombinedMatrix(1 , 1:(coloumnsA1*rowsA1));
+
+VHDext = CombinedMatrix(1 , (coloumnsA1*rowsA1 + 1):colsComMat);
+
+A1deQuantized = reshape(A1ext, rowsA1 , coloumnsA1) * q;
+
+decodedVHD = run_length_encode(VHDext,'decode');
+
+VHDTraversed = inverseSpiralTraversal(decodedVHD, rowsA1 , coloumnsA1*3);
+
+V1deQuantized = VHDTraversed(1:rowsA1 , 1:coloumnsA1) * q;
+
+H1deQuantized = VHDTraversed(1:rowsA1 , (coloumnsA1 + 1) : coloumnsA1*2) * q;
+
+D1deQuantized = VHDTraversed(1:rowsA1 , (coloumnsA1*2 + 1) : coloumnsA1*3) * q;
+
+if isfield(metadata, 'BitsAllocated')
+    bitsAllocated = metadata.BitsAllocated;
+    if bitsAllocated == 16
+        if isfield(metadata, 'PixelRepresentation') && metadata.PixelRepresentation == 1
+            dicomClass = @int16;
+        else
+            dicomClass = @uint16;
+        end
+    else
+        error('Error: Invalid DICOM class. Only supports uint16 and int16');
+    end
+else
+    error('Error: Bit depth not found');
+end
+
+reconstructedImage = dicomClass(idwt2(A1deQuantized,H1deQuantized,V1deQuantized,D1deQuantized,w));
+MSECHK = immse(img,reconstructedImage);
+
+diffImage = imabsdiff(img,reconstructedImage);
+meanDiff = mean(diffImage(:));
+
+[peaksnr, snr] = psnr(reconstructedImage, img);
+
+encMat = dir('ImgData1.bin');
+encMatSize = encMat.bytes;
+
+fileNameMetadata = 'metadata.mat';
+metadatadir = dir(fileNameMetadata);
+metdadataSize = metadatadir.bytes;
+encMatSize = encMat.bytes + metdadataSize;
+compressionRatio = orgSize/encMatSize;
+
+MSECHK =  round(vpa(sym(MSECHK), 4),4);
+meanDiff =  round(vpa(sym(meanDiff), 4),4);
+peaksnr =  round(vpa(sym(peaksnr), 4),4);
+compressionRatio = round(vpa(sym(compressionRatio), 4),4);
+
+RMSE = sqrt(MSECHK);
+SSIM = ssim(img,reconstructedImage);
+
+RMSE =  round(vpa(sym(RMSE), 4),4);
+
+targetMSE
+MSECHK
+RMSE
+meanDiff
+peaksnr
+SSIM
+compressionRatio
+
+cd(currentDir)
+
+% Stop the timer
+elapsedTime = toc
+
+%%
+%RLE
+function output = run_length_encode(data, type)
+if strcmp(type, 'encode')
+    output = [];
+    i = 1;
+    while i <= length(data)
+        count = 1;
+        while (i+count) <= length(data) && data(i+count) == data(i)
+            count = count + 1;
+        end
+        output = [output, data(i), count];
+        i = i + count;
+    end
+elseif strcmp(type, 'decode')
+    output = [];
+    for i = 1:2:length(data)-1
+        element = data(i);
+        count = data(i+1);
+        output = [output, repmat(element, 1, count)];
+    end
+else
+    error('Invalid encoding type');
+end
+end
+
+
+
+%%
+%Spiral Trverasal
+function output = spiralTraversal(matrix)
+[rows, cols] = size(matrix);
+output = []
+
+% Initialize variables
+rowStart = 1;
+rowEnd = rows;
+colStart = 1;
+colEnd = cols;
+
+while rowStart <= rowEnd && colStart <= colEnd
+    for col = colStart:colEnd
+
+        output = [output , matrix(rowStart, col)];
+
+    end
+    rowStart = rowStart + 1;
+
+    for row = rowStart:rowEnd
+
+        output = [output , matrix(row, colEnd)];
+
+    end
+    colEnd = colEnd - 1;
+
+    if rowStart <= rowEnd
+        for col = colEnd:-1:colStart
+
+            output = [output , matrix(rowEnd, col)];
+
+        end
+        rowEnd = rowEnd - 1;
+    end
+
+    if colStart <= colEnd
+        for row = rowEnd:-1:rowStart
+
+            output = [output , matrix(row, colStart)];
+
+        end
+        colStart = colStart + 1;
+    end
+end
+end
+
+
+%%
+%Inverse Spiral Trverasal
+
+function output = inverseSpiralTraversal(matrix,rows, cols)
+
+output = zeros(rows, cols);
+
+rowStart = 1;
+rowEnd = rows;
+colStart = 1;
+colEnd = cols;
+index = 1;
+
+while rowStart <= rowEnd && colStart <= colEnd
+    for col = colStart:colEnd
+
+        output(rowStart, col) = matrix(index);
+
+        index = index + 1;
+
+    end
+    rowStart = rowStart + 1;
+
+    for row = rowStart:rowEnd
+
+        output(row, colEnd) = matrix(index);
+
+        index = index + 1;
+
+    end
+    colEnd = colEnd - 1;
+
+    if rowStart <= rowEnd
+        for col = colEnd:-1:colStart
+
+            output(rowEnd, col) = matrix(index);
+
+            index = index + 1;
+
+        end
+        rowEnd = rowEnd - 1;
+    end
+
+    if colStart <= colEnd
+        for row = rowEnd:-1:rowStart
+
+            output(row, colStart) = matrix(index);
+
+            index = index + 1;
+
+        end
+        colStart = colStart + 1;
+    end
+end
+end
---
+++ Source code/Thresh_rate/Thresh_Rate_Lin_Trav.m
@@ -0,0 +1,224 @@
+clc
+clear all
+close all
+
+% Matlab script for outputing compression metrics for a given 16bit DICOM
+% image and for threshold rate values from 0.2 to 0.99 with a step of 0.03.
+% Here Adaptive quantization with Linear Traversal is used.
+% File paths should be given as input parameters.
+
+% Start the timer
+tic
+
+fileNames = {
+    ' ', ' ', ' '
+    };
+
+for i = 1:numel(fileNames)
+
+    % Clear workspace
+    clearvars -except fileNames i;
+
+    fileName = fileNames{i}
+
+    s = dir(fileName);
+    orgSize = s.bytes
+
+    img = dicomread(fileName);
+    class = class(img)
+
+    metadata = dicominfo(fileName);
+
+    if strcmp(class, 'uint16')
+
+        dicomClass = @uint16;
+
+    elseif strcmp(class, 'int16')
+
+        dicomClass = @int16;
+
+    else
+        error('Error: Invalid DICOM class. Only supports uint16 and int16');
+    end
+
+    w = 'db1'
+    n = 1;
+    [C,S] = wavedec2(img,n,w);
+
+    Csort = sort(abs(C(:)));
+
+    A1 = appcoef2(C,S,w,1);
+    [H1 V1 D1] = detcoef2('all',C,S,1);
+
+    iterationMat = [];
+    RMSEMat = [];
+    peaksnrMat = [];
+    absDiffMat = [];
+    compressionRatioMat = [];
+    ssimMat = [];
+    MSEMargin = 0.05
+    q = 0;
+
+    for n= 0.2: 0.03 : 0.99
+
+        keep1 = [n];
+
+        thresh = Csort(round((1-keep1)*length(Csort)));
+
+        for p= 5: 1 : 40
+
+            q = p;
+
+            indA1 = abs(A1)>thresh;
+            indH1 = abs(H1)>thresh;
+            indV1 = abs(V1)>thresh;
+            indD1 = abs(D1)>thresh;
+
+            A1filt = A1.*indA1;
+            H1filt = H1.*indH1;
+            V1filt = V1.*indV1;
+            D1filt = D1.*indD1;
+
+            reconstructedImage = dicomClass(idwt2(A1filt,H1filt,V1filt,D1filt,w));
+            MSE1 = immse(img,reconstructedImage);
+
+            A1filt = round(A1filt/q);
+            H1filt = round(H1filt/q);
+            V1filt = round(V1filt/q);
+            D1filt = round(D1filt/q);
+
+            A1filtNew = A1filt * q;
+            V1reshaped = V1filt * q;
+            H1reshaped = H1filt * q;
+            D1reshaped = D1filt * q;
+            reconstructedImage = dicomClass(idwt2(A1filtNew,H1reshaped,V1reshaped,D1reshaped,w));
+
+            MSE2 = immse(img,reconstructedImage);
+
+            if MSE2>= (MSE1 + MSEMargin*MSE1*n)
+
+                break;
+            end
+
+        end
+
+        reshapedA1 = reshape(A1filt, 1 , []);
+
+        VHD = [V1filt H1filt D1filt];
+
+        reshapedVHD = reshape(VHD, 1 , []);
+
+        rleEncodedVHD = run_length_encode(reshapedVHD,'encode');
+
+        CombinedMatrix = [reshapedA1 rleEncodedVHD];
+
+        rowsA1 = size(A1filt,1);
+
+        coloumnsA1  = size(A1filt,2);
+
+        writematrix(CombinedMatrix);
+
+        save("data.mat","q","metadata","rowsA1","coloumnsA1");
+        gzip('CombinedMatrix.txt');
+        openfile = fopen("CombinedMatrix.txt.gz","r");
+        data = fread(openfile);
+        OpenDatafileID = fopen('ImgData1.bin', 'w');
+        fwrite(OpenDatafileID, data);
+
+        % Close the file
+        fclose(OpenDatafileID);
+        fclose(openfile);
+        delete('CombinedMatrix.txt',"CombinedMatrix.txt.gz");
+        rowsA1 = size(A1filt,1);
+        coloumnsA1  = size(A1filt,2);
+        colsComMat = size(CombinedMatrix,2);
+        A1ext = CombinedMatrix(1 , 1:(coloumnsA1*rowsA1));
+        VHDext = CombinedMatrix(1 , (coloumnsA1*rowsA1 + 1):colsComMat);
+        A1filtNew = reshape(A1ext, rowsA1 , coloumnsA1) * q;
+        decodedVHD = run_length_encode(VHDext,'decode');
+        VHDreshaped = reshape(decodedVHD, rowsA1 , coloumnsA1*3);
+        V1filtNew = VHDreshaped(1:rowsA1 , 1:coloumnsA1) * q;
+        H1filtNew = VHDreshaped(1:rowsA1 , (coloumnsA1 + 1) : coloumnsA1*2) * q;
+        D1filtNew = VHDreshaped(1:rowsA1 , (coloumnsA1*2 + 1) : coloumnsA1*3) * q;
+
+        reconstructedImage = dicomClass(idwt2(A1filtNew,H1filtNew,V1filtNew,D1filtNew,w));
+        MSE = immse(img,reconstructedImage);
+
+        diffImage = imabsdiff(img,reconstructedImage);
+        meanDiff = mean(diffImage(:));
+        [peaksnr, snr] = psnr(reconstructedImage, img);
+
+        encMat = dir('ImgData1.bin');
+        encMatSize = encMat.bytes;
+
+        fileNameMetadata = 'data.mat';
+        metadatadir = dir(fileNameMetadata);
+        metdadataSize = metadatadir.bytes;
+
+        encMatSize = encMat.bytes + metdadataSize;
+
+        compressionRatio = orgSize/encMatSize;
+
+        itr =  round(vpa(sym(n), 4),4);
+        SSIM = ssim(reconstructedImage,img);
+        RMSE = sqrt(MSE);
+        RMSE =  round(vpa(sym(RMSE), 4),4);
+        meanDiff =  round(vpa(sym(meanDiff), 4),4);
+        peaksnr =  round(vpa(sym(peaksnr), 4),4);
+        compressionRatio = round(vpa(sym(compressionRatio), 4),4);
+
+        iterationMat = [iterationMat; itr];
+        RMSEMat = [RMSEMat; RMSE];
+        absDiffMat = [absDiffMat; meanDiff];
+        peaksnrMat = [peaksnrMat; peaksnr];
+        compressionRatioMat = [compressionRatioMat; compressionRatio];
+        ssimMat = [ssimMat; SSIM];
+
+    end
+
+    iterationMat_numeric = double(iterationMat);
+    RMSEMat_numeric = double(RMSEMat);
+    absDiffMat_numeric = double(absDiffMat);
+    peaksnrMat_numeric = double(peaksnrMat);
+    ssimMat_numeric = double(ssimMat);
+    compressionRatioMat_numeric = double(compressionRatioMat);
+
+    % Write matrices to Excel file
+    writematrix(iterationMat_numeric, 'data.xlsx', 'Sheet', i, 'Range', 'A1');
+    writematrix(RMSEMat_numeric, 'data.xlsx', 'Sheet', i, 'Range', 'B1');
+    writematrix(absDiffMat_numeric, 'data.xlsx', 'Sheet', i, 'Range', 'C1');
+    writematrix(peaksnrMat_numeric, 'data.xlsx', 'Sheet', i, 'Range', 'D1');
+    writematrix(ssimMat_numeric, 'data.xlsx', 'Sheet', i, 'Range', 'E1');
+    writematrix(compressionRatioMat_numeric, 'data.xlsx', 'Sheet', i, 'Range', 'F1');
+
+end
+
+
+% Stop the timer
+elapsedTime = toc
+
+%%
+%RLE
+function output = run_length_encode(data, type)
+if strcmp(type, 'encode')
+    output = [];
+    i = 1;
+    while i <= length(data)
+        count = 1;
+        while (i+count) <= length(data) && data(i+count) == data(i)
+            count = count + 1;
+        end
+        output = [output, data(i), count];
+        i = i + count;
+    end
+elseif strcmp(type, 'decode')
+    output = [];
+    for i = 1:2:length(data)-1
+        element = data(i);
+        count = data(i+1);
+        output = [output, repmat(element, 1, count)];
+    end
+else
+    error('Invalid encoding type');
+end
+end
